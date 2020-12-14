06eaf9557527f3416c6ef2d6a67f867bce8f5347
abstract_arrow <ramtintoosi@gmail.com>
2020-12-14 12:37:53 +0330

    Add initial template

---
+++ android/app/src/main/kotlin/xyz/devura/shoplifter/MainActivity.kt
@@ -0,0 +1,6 @@
+package xyz.devura.shoplifter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Shoplifter());
+}
+
+class Shoplifter extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'shoplifter',
+      home: Scaffold(),
+    );
+  }
+}
